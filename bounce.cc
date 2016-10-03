4fe048f24f690f2d8b725818db802207c25cfe9a
Kostas Kouvaris <kk6g11@soton.ac.uk>
2016-10-03 12:13:17 +0100

    improves abstraction and readability. inserts functions.

--- bounce.cc
+++ bounce.cc
@@ -2,21 +2,23 @@
 
 #include <iostream>
 
-int main() {
+int minColumn = 0;
+int maxColumn = 80;
+char particleSymbol = 'x';
+double particlePosition = minColumn;
+double particleSpeed = 6.3;
 
-  int minColumn = 0;
-  int maxColumn = 80;
-  char particleSymbol = 'x';
-  double particlePosition = minColumn;
-  double particleSpeed = 6.3;
-  int timeStep = 0;
-  int stopTime = 60;
-
-  while (timeStep < stopTime) {
+void draw(){
+	//draw start  
     for (int i = 0; i < particlePosition; i++) {
       std::cout << " ";
     }
     std::cout << particleSymbol << std::endl;
+	//draw end	
+}
+
+void move(){
+  //move start
     particlePosition += particleSpeed;
     if (particlePosition >= maxColumn) {
       particlePosition = maxColumn;
@@ -24,7 +26,20 @@ int main() {
     } else if (particlePosition < minColumn) {
       particlePosition = minColumn;
       particleSpeed = -particleSpeed;
-    }    
+    }
+	//move stop
+}
+
+int main() {
+	
+  int timeStep = 0;
+  int stopTime = 60;
+
+  while (timeStep < stopTime) {
+
+	draw();
+	move();
+
     timeStep++;
   }
 }
